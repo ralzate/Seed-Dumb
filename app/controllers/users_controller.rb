428e0e1fde9a48e782173fa8973d253abea014aa
alejoalzate <alejoalzate12@gmail.com>
2015-12-16 07:36:15 -0500

    Arreglo error show-edit usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :current_user?, only: [:edit, :update, :destroy]
   skip_before_filter :require_login, only: [:new, :create, :show]
-  # before_action :authorize
-
   # GET /users
   # GET /users.json
   def index
@@ -29,7 +27,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { login(params[:user][:nombre_usuario], params[:user][:password])
+        format.html { login(params[:user][:email], params[:user][:password])
                       flash[:success] = "Registration successful. Please check your email for activation."
                       redirect_to root_path  }
         format.json { render :show, status: :created, location: @user }
@@ -80,16 +78,14 @@ class UsersController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.where(nombres: params[:id]).first
-      unless @user
-        return redirect_back_or_to root_path, notice: 'could not find user'
-      end
+      @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:nombre_usuario, :nombres, :apellidos , :tipo_documento, :documento, :rol_id,
-      :registro_medico, :email, :password, :password_confirmation, :picture)
+      params.require(:user).permit(:names, :surnames , :type_document, :document, :rol_id,
+      :medical_record, :email, :password, :password_confirmation, :picture)
     end
 end
