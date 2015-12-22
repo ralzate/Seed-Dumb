604a4b837dcf4604d35477532aeb04d69c707dac
alejoalzate <alejoalzate12@gmail.com>
2015-12-22 15:41:22 -0500

    Sin los Chulitos

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,6 @@ PetThing::Application.routes.draw do
   end
 
 
-  get       'signup', to:   'users#new',  as: :signup
   get       'login',  to:   'sessions#new',       as: :login
   post      'login',  to:   'sessions#create'
   delete    'logout', to:   'sessions#destroy',   as: :logout
